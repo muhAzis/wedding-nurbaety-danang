04a2d029b6655a4319d9aee3e92eb1a1cf98f12e
azis <muhabdulazis555@gmail.com>
2023-09-10 00:41:33 +0700

    fix sorting comments

--- firebase.js
+++ firebase.js
@@ -33,7 +33,7 @@ const getData = async () => {
     }
 
     const dataComments = Object.keys(data.val()).map((key) => data.val()[key]);
-    dataComments.sort((a, b) => a.issued - b.issued);
+    dataComments.sort((a, b) => a.issued < b.issued); //sort by latest posting date
 
     commentsContainer.innerHTML = '';
     dataComments.forEach((data) => {
